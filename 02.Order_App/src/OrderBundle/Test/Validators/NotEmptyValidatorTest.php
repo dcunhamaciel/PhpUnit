f9fd5256f063aacc648a07a6e6700228851d9663
dcunhamaciel <dcunhamaciel@gmail.com>
2022-09-03 13:54:39 -0300

    Teste de Valor

---
+++ 02.Order_App/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OrderBundle\Validators\Test;
+
+use PHPUnit\Framework\TestCase;
+use OrderBundle\Validators\NotEmptyValidator;
+
+class NotEmptyValidatorTest extends TestCase 
+{
+    public function testShouldNotBeValidWhenValueIsEmpty()
+    {
+        $emptyValue = "";
+
+        $notEmptyValidator = new NotEmptyValidator($emptyValue);
+
+        $isValid = $notEmptyValidator->isValid();
+
+        $this->assertFalse($isValid);
+    }
+
+    public function testShouldBeValidWhenValueIsNotEmpty()
+    {
+        $notEmptyValue = "Valid Value";
+
+        $notEmptyValidator = new NotEmptyValidator($notEmptyValue);
+
+        $isValid = $notEmptyValidator->isValid();
+
+        $this->assertTrue($isValid);
+    }
+}
